e634d6b2768017513af096f1e424d533a231271a
Sheldrick Mayrant <mrmayrant@yahoo.com>
2022-05-12 17:49:43 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/main.scss";
+import logo from "./img/logo.png";
+
+const logoImg = document.querySelector(".header__logo");
+logoImg.src = logo;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "src"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "src/styles"),
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            include: path.resolve(__dirname, "src"),
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g)$/i,
+        type: "asset",
+        include: path.resolve(__dirname, "src/img"),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Portfolio",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
